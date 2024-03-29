2deae698f08a65ccd0f50c4b7f617761b6f319d7
Marcos Cabral Peralta <156668187+Marcos-93LAB@users.noreply.github.com>
2024-03-29 20:30:58 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [selectedColor, setSelectedColor] = useState ("red");
+	const [purpleActivo, setPurpleActive] = useState (false);
+
+	const cambiaColor = () => {
+		if (selectedColor === "red") {
+			setSelectedColor("yellow");
+		} else if (selectedColor === "yellow") {
+			setSelectedColor("green");
+		} else if (selectedColor === "green"){
+			setSelectedColor("red");
+		} else {
+			if (selectedColor === "red") {
+				setSelectedColor("yellow");
+			} else if (selectedColor === "yellow") {
+				setSelectedColor("green");
+			} else if (selectedColor === "green"){
+				setSelectedColor("purple");
+			} else if (selectedColor === "purple"){
+				setSelectedColor("red");
+			}
+		};
+	};
+		
+	
+
+	const botonPurple = () =>{
+		setPurpleActive(!purpleActivo);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="stick"></div>
+			<div className="traffic-light">
+				<div 
+				onClick={() => setSelectedColor("red")}
+				className={"light red" + ((selectedColor === "red") ? " glow" : "")}>
+				</div>
+				<div
+				onClick={() => setSelectedColor("yellow")} 
+				className={"light yellow" + ((selectedColor === "yellow") ? " glow" : "")}>
+				</div>
+				<div 
+				onClick={() => setSelectedColor("green")}
+				className={"light green" + ((selectedColor === "green") ? " glow" : "")}>
+				</div>
+				{purpleActivo && <div 
+				onClick={() => setSelectedColor("purple")}
+				className={"light purple" + ((selectedColor === "purple") ? " glow" : "")}>
+				</div>}
+			</div>
+			<div>
+				<button onClick={cambiaColor}>Cambiar color</button>
+				<button onClick={botonPurple}>Purpura</button>
+			</div>
 		</div>
 	);
 };
